047a0acaaa1f6d2ab75ad44c008a0107a324d2b9
rahmadila1038 <48636971+rahmadila1038@users.noreply.github.com>
2020-11-18 12:38:48 +0700

    Update StoryActivity.java

--- app/src/main/java/com/example/secretdiary/StoryActivity.java
+++ app/src/main/java/com/example/secretdiary/StoryActivity.java
@@ -2,13 +2,29 @@ package com.example.secretdiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StoryActivity extends AppCompatActivity {
+public class StoryActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Button btnKembali = (Button) findViewById(R.id.btn_kembali);
+        btnKembali.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_kembali:
+            Intent btn_kembali = new Intent(StoryActivity.this, MainActivity.class);
+            startActivity(btn_kembali);
+
+        }
     }
 }
