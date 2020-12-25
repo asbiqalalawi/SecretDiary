5a04ed04a57ac5678c3bb8245d777a9659d67e39
rahmadila1038 <rahmadilan11@gmail.com>
2020-12-25 19:49:11 +0700

    update layout

---
+++ app/src/main/java/com/example/secretdiary/BuatPassActivity.java
@@ -0,0 +1,14 @@
+package com.example.secretdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuatPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_pass);
+    }
+}
---
+++ app/src/main/java/com/example/secretdiary/PengingatActvity.java
@@ -0,0 +1,14 @@
+package com.example.secretdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PengingatActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengingat_actvity);
+    }
+}
